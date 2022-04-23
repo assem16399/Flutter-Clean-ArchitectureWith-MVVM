1f57a8aef3d3a0bc36ee3bf23d56a043b9209d4a
Aasem <assem_soma541@yahoo.com>
2022-04-23 13:05:04 +0200

    App Bar Theme Created

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,4 +1,6 @@
 import 'package:clean_architecture_mvvm/presentation/color_manager.dart';
+import 'package:clean_architecture_mvvm/presentation/font_manager.dart';
+import 'package:clean_architecture_mvvm/presentation/styles_manager.dart';
 import 'package:clean_architecture_mvvm/presentation/values_manager.dart';
 import 'package:flutter/material.dart';
 
@@ -8,10 +10,21 @@ ThemeData get appTheme {
     primaryColorLight: ColorManager.lightPrimary,
     primaryColorDark: ColorManager.darkPrimary,
     disabledColor: ColorManager.grey1,
+    splashColor: ColorManager.lightPrimary,
     cardTheme: CardTheme(
       color: ColorManager.white,
       shadowColor: ColorManager.grey,
       elevation: AppSize.s4,
+    ),
+    appBarTheme: AppBarTheme(
+      elevation: AppSize.s4,
+      backgroundColor: ColorManager.primary,
+      foregroundColor: ColorManager.white,
+      shadowColor: ColorManager.lightPrimary,
+      titleTextStyle: getRegularStyle(
+        color: ColorManager.white,
+        fontSize: FontSize.s16,
+      ),
     ),
   );
 }
