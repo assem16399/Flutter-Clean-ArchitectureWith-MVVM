ac1c708c62171011698622c63842956d55f6630d
Aasem <assem_soma541@yahoo.com>
2022-04-23 11:09:54 +0200

    Colors Manager Class Created

---
+++ lib/presentation/colors_manager.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+abstract class ColorsManager {
+  static Color primary = const Color(0XFFED9728);
+  static Color darkGrey = const Color(0XFF525252);
+  static Color grey = const Color(0XFF737477);
+  static Color lightGrey = const Color(0XFF9E9E9E);
+}
