f950b5146ad95b941d1bf23bdbc17a634b011fd9
Aasem <assem_soma541@yahoo.com>
2022-04-23 12:56:26 +0200

    Theme Manager Created

--- lib/presentation/color_manager.dart
+++ lib/presentation/color_manager.dart
@@ -5,4 +5,10 @@ abstract class ColorManager {
   static Color darkGrey = const Color(0XFF525252);
   static Color grey = const Color(0XFF737477);
   static Color lightGrey = const Color(0XFF9E9E9E);
+  static Color darkPrimary = const Color(0XFFd17d11);
+  static Color lightPrimary = const Color(0XCCd17d11);
+  static Color grey1 = const Color(0XFF707070);
+  static Color grey2 = const Color(0XFF797979);
+  static Color white = const Color(0XFFFFFFFF);
+  static Color error = const Color(0XFFe61f34);
 }
